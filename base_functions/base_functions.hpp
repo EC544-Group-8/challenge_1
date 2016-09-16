28925863bd3ea735f55fe69e68298a49dd5414bd
Luke Osborne <losborne@bu.edu>
2016-09-15 21:59:03 -0400

    base functions created and avg function implemented and tested

---
+++ base_functions/base_functions.hpp
@@ -0,0 +1,19 @@
+/*******************************
+ * Functions used by base XBee
+ * base_functions.hpp
+ *
+ * Boston University
+ * EC544 Fall 2016 - Group 8
+ * Andrew Delollis, Connor McCann, Eric Mooney, Luke Osborne
+ *
+ * Challenge 1
+ */
+
+#ifndef BASE_FUNCTIONS_HPP
+#define BASE_FUNCTIONS_HPP
+
+#include <vector>
+
+double average_temp(const std::vector<double>& temps);
+
+#endif
