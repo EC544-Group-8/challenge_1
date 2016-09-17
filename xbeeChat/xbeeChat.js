d236f2ed27f33053e1f7b132cef890cc755234f2
Connor McCann <cjmccann@bu.edu>
2016-09-17 14:26:07 -0400

    print every 2 seconds

--- xbeeChat/xbeeChat.js
+++ xbeeChat/xbeeChat.js
@@ -32,8 +32,62 @@ http.listen(3000, function(){
 sp.on("open", function () {
   console.log('open');
   sp.on('data', function(data) {
-    console.log('data received: ' + data);
+    //console.log('data received: ' + data);
     io.emit("chat message", "An XBee says: " + data);
+    // Parsing
+    parse_data(data);
+    calc_avg(measurement);
   });
 });
+
+//==========================================================================
+var avg = 0;
+
+var measurement = {
+  '1': '0',
+  '2': '0',
+  '3': '0',
+  '4': '0'
+};
+
+function parse_data(dataString) {
+  var arrayOfStrings = dataString.split(':');
+  if(arrayOfStrings.length == 2) {
+    var id = arrayOfStrings[0];
+    //idArray.push(parseInt(id));
+    //console.log(idArray);
+
+    var temp = arrayOfStrings[1];
+    measurement[id] = temp;
+    //console.log('HERE');
+    //for(i = 1; i < 5; i++){
+      //console.log(i +'  '+measurement[i])
+    //}
+  } 
+}
+
+function calc_avg(allData){
+  var total = 0;
+  for(i = 1; i < 5; i++){
+    total += parseFloat(allData[i]);
+  }
+  avg = total/4;
+  //console.log('The Average is:   ' + avg);
+}
+
+function print_data(avgTemp){
+    console.log('The Average is:   ' + avgTemp);
+}
+
+
+
+
+setInterval(function(){
+    print_data(avg);
+},2000);
+
+
+
+
+
 
